3da9e754495dc628572457c25c18fb48a2a9b94b
jfjeffries <jon.f.jeffries@gmail.com>
2018-07-10 16:24:54 -0400

    jfj added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
 import { AboutComponent } from './components/about/about.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
